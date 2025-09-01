419a5cfbee64081a241b220e09cd468b50c91838
my umair <@myumair.com>
2025-09-01 21:31:46 +0700

    ditambahkan

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ print("belajar dasar git")
 # git add [nama_file_yg_berubah]
 # git comit -m "isi_pesan_perubahan"
 # git push -u origin main
+#--- cara nambah app.py file --
+# git add app.py
+# git comit -m 
+#git push -u origin main
